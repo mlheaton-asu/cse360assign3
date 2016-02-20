5566263b1ed97163055f37c53c9fa4a2cf283fe4
mlheaton-asu <mlheaton@asu.edu>
2016-02-19 19:58:48 -0700

    Initial version.

---
+++ SoftwareENG/src/cse360assign3/Calculator.java
@@ -0,0 +1,57 @@
+/**
+ * Description: The purpose of this class is to act as a basic calculator.
+ * Name: Michael Heaton
+ * PIN: 404
+ * */
+
+package cse360assign3;
+
+public class Calculator {
+
+	private int total;
+	
+	/**
+	 * This is the default constructor for the class Calculator.
+	 * @param none*/
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * The purpose of this method is to return the total value resulting 
+	 * from calculations.*/
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * The purpose of this method is to add a value.*/
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * The purpose of this method is to subtract a value.*/
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * The purpose of this method is to multiply a value.*/
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * The purpose of this method is to divide a value.*/
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * The purpose of this method is to put the calculator history into 
+	 * string form.*/
+	public String getHistory () {
+		return "";
+	}
+}
